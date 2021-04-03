ad8469f6347eb8cca5edcdc45665624796b93599
fighantalenta_ <tatamf.id@gmail.com>
2021-04-03 16:17:19 +0700

    fix bugs

--- src/app.js
+++ src/app.js
@@ -21,8 +21,6 @@ app.use(express.static("public"));
 
 app.use("/users", users);
 
-app.us;
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log("server running on port" + PORT));
