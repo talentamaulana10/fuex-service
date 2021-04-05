85725e3c42fd3b1fa324babae96391d33b638289
fighantalenta_ <tatamf.id@gmail.com>
2021-04-05 15:00:31 +0700

    fix cors

--- src/app.js
+++ src/app.js
@@ -9,6 +9,15 @@ const users = require("./controllers/User");
 
 const path = require("path");
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 mongoose
   .connect(config, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Mongodb Successfuly Connect !"))
