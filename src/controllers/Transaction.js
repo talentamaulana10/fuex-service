429c17cabb906bbb313a364f20532ee3add59f9c
fighantalenta_ <tatamf.id@gmail.com>
2021-04-12 14:08:50 +0700

    transaction pending

--- src/controllers/Transaction.js
+++ src/controllers/Transaction.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const Product = require("../models/Product");
 const Transaction = require("../models/Transaction");
 const responseTemplate = require("../response-templates");
 
@@ -23,26 +25,41 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
+  var {
+    vehicleName,
+    vehicleType,
+    numberPlate,
+    buyer,
+    product,
+    deliveryCost,
+    address,
+    adminFee,
+  } = req.body;
   try {
-    const newTransaction = new Transaction({
-      vehicleName: req.body.vehicleName,
-      vehicleType: req.body.vehicleType,
-      numberPlate: req.body.numberPlate,
-    });
-    const DBTransactionInteraction = await newTransaction.save();
-    if (DBTransactionInteraction) {
-      const templateResponse = responseTemplate.success;
-      templateResponse.data = DBTransactionInteraction;
-      res.status(200).json(templateResponse);
+    const DBUserTypeInteraction = await User.findById(buyer);
+    if (DBUserTypeInteraction) {
+      try {
+        const DBFuelTypeInteraction = await FuelType.findById(fuelType);
+        if (DBFuelTypeInteraction) {
+        } else {
+          const templateResponse = responseTemplate.error;
+          templateResponse.message = "FUEL TYPE NOT FOUND";
+          res.status(200).json(templateResponse);
+        }
+      } catch (error) {
+        const templateResponse = responseTemplate.error;
+        templateResponse.message = error;
+        res.status(200).json(templateResponse);
+      }
     } else {
       const templateResponse = responseTemplate.error;
-      templateResponse.message = DBTransactionInteraction;
+      templateResponse.message = "USER NOT FOUND";
       res.status(200).json(templateResponse);
     }
   } catch (error) {
     const templateResponse = responseTemplate.error;
     templateResponse.message = error;
-    res.json(templateResponse);
+    res.status(200).json(templateResponse);
   }
 });
 
