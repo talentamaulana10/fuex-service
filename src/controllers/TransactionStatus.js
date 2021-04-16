e27e10af7d2972456259d694cb0e1443b2e1a0b7
fighantalenta_ <tatamf.id@gmail.com>
2021-04-16 17:34:32 +0700

    trx status

--- src/controllers/TransactionStatus.js
+++ src/controllers/TransactionStatus.js
@@ -22,6 +22,26 @@ router.get("/", async (req, res) => {
   }
 });
 
+
+router.get("/:id", async (req, res) => {
+  try {
+    const DBTransactionStatusInteraction = await TransactionStatus.findById(req.params.id);
+    if (DBTransactionStatusInteraction) {
+      const templateResponse = responseTemplate.success;
+      templateResponse.data = DBTransactionStatusInteraction;
+      res.status(200).json(templateResponse);
+    } else {
+      const templateResponse = responseTemplate.error;
+      templateResponse.message = "DATA NOT FOUND";
+      res.status(200).json(templateResponse);
+    }
+  } catch (error) {
+    const templateResponse = responseTemplate.error;
+    templateResponse.message = ` ${error}`;
+    res.json(templateResponse);
+  }
+});
+
 router.post("/", async (req, res) => {
   try {
     const newTransactionStatus = new TransactionStatus({
