9dc8745baa34e9b9fec0f1927dbddb40c5fbc7a9
Prasetya234 <putraprasetyadhany@gmail.com>
2021-04-24 09:47:22 +0700

    fixingPathCancle

--- src/app.js
+++ src/app.js
@@ -21,9 +21,9 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
   next();
 });
-
 mongoose
   .connect(config, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Mongodb Successfuly Connect !"))
--- src/controllers/Transaction.js
+++ src/controllers/Transaction.js
@@ -45,16 +45,94 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.delete("/cancleOrede/:id", async (req, res) => {
-  const id = req.params.id;
+router.put("/cancel/:id", async (req, res) => {
   try {
-    const CancleOrede = await Transaction.findByIdAndRemove(id);
-    if (CancleOrede) {
-      res.status(404).send({
-        message: `Cannot delete Transaction with id=${id}. Maybe Transaction was not found!`,
-      });
-    } else {
-      res.send({ message: "Transaction was deleted successfully!" });
+    const findTransactionById = await Transaction.findById(req.params.id);
+    if (!findTransactionById) {
+      const templateResponse = responseTemplate.error;
+      templateResponse.message = "TRANSACTION NOT FOUND";
+      res.status(200).json(templateResponse);
+      return;
+    }
+    if (findTransactionById.status !== "1") {
+      const templateResponse = responseTemplate.error;
+      templateResponse.message = "TRANSACTION CANNOT TO CANCEL";
+      res.status(200).json(templateResponse);
+      return;
+    }
+    try {
+      const FindProductById = await Product.findById(
+        findTransactionById.product
+      );
+      if (!FindProductById) {
+        const templateResponse = responseTemplate.error;
+        templateResponse.message = "PRODUCT NOT FOUND";
+        res.status(200).json(templateResponse);
+        return;
+      }
+      try {
+        const findUserBuyerById = await User.findById(
+          findTransactionById.buyer
+        );
+        if (!findUserBuyerById) {
+          const templateResponse = responseTemplate.error;
+          templateResponse.message = "BUYER NOT FOUND";
+          res.status(200).json(templateResponse);
+          return;
+        }
+        try {
+          const UpdateUserWallet = await Wallet.findByIdAndUpdate(
+            findUserBuyerById.wallet,
+            {
+              $inc: {
+                balance:
+                  +FindProductById.price +
+                  findTransactionById.deliveryCost +
+                  findTransactionById.adminFee * findTransactionById.quantity,
+              },
+            }
+          );
+          if (!UpdateUserWallet) {
+            const templateResponse = responseTemplate.error;
+            templateResponse.message = "FAILED UPDATE USER WALLET";
+            res.status(200).json(templateResponse);
+            return;
+          }
+          try {
+            const SetOrderStatus = await Transaction.findByIdAndUpdate(
+              req.params.id,
+              {
+                status: "4",
+              }
+            );
+            if (!SetOrderStatus) {
+              const templateResponse = responseTemplate.error;
+              templateResponse.message = "FAILED UPDATE ORDER STATUS";
+              res.status(200).json(templateResponse);
+              return;
+            }
+            const templateResponse = responseTemplate.success;
+            templateResponse.data = SetOrderStatus;
+            res.status(200).json(templateResponse);
+          } catch (error) {
+            const templateResponse = responseTemplate.error;
+            templateResponse.message = `${error}`;
+            res.status(200).json(templateResponse);
+          }
+        } catch (error) {
+          const templateResponse = responseTemplate.error;
+          templateResponse.message = `${error}`;
+          res.status(200).json(templateResponse);
+        }
+      } catch (error) {
+        const templateResponse = responseTemplate.error;
+        templateResponse.message = `${error}`;
+        res.status(200).json(templateResponse);
+      }
+    } catch (error) {
+      const templateResponse = responseTemplate.error;
+      templateResponse.message = `${error}`;
+      res.status(200).json(templateResponse);
     }
   } catch (error) {
     const templateResponse = responseTemplate.error;
@@ -63,80 +141,179 @@ router.delete("/cancleOrede/:id", async (req, res) => {
   }
 });
 
-// router.put("/completion/:id", async (req, res) => {
-//   try {
-//     const DBTransactionInteraction = await Transaction.findById(req.params.id);
-//     if (DBTransactionInteraction) {
-//       try {
-//         const DBProductInteraction = await Product.findById(
-//           DBTransactionInteraction.product
-//         );
-//         if (DBProductInteraction) {
-//           try {
-//             const DBResiInteraction = await Resi.findById(
-//               DBTransactionInteraction.resi
-//             );
-//             if (DBResiInteraction) {
-//               try {
-//                 const CheckingUserWalletSeller = await User.findById(
-//                   DBResiInteraction.sender
-//                 );
-//                 if (CheckingUserWalletSeller) {
-//                   try {
-//                     var hasilPenjual = (DBProductInteraction.price * 98) / 100;
-//                     try {
-//                       const CheckingUserWallet;
-//                     } catch (error) {
-//                       const templateResponse = responseTemplate.error;
-//                       templateResponse.message = `${error}`;
-//                       res.status(200).json(templateResponse);
-//                     }
-//                   } catch (error) {
-//                     const templateResponse = responseTemplate.error;
-//                     templateResponse.message = `${error}`;
-//                     res.status(200).json(templateResponse);
-//                   }
-//                 } else {
-//                   const templateResponse = responseTemplate.error;
-//                   templateResponse.message = "USER TIDAK DITEMUKAN";
-//                   res.status(200).json(templateResponse);
-//                 }
-//               } catch (error) {
-//                 const templateResponse = responseTemplate.error;
-//                 templateResponse.message = `${error}`;
-//                 res.status(200).json(templateResponse);
-//               }
-//             } else {
-//               const templateResponse = responseTemplate.error;
-//               templateResponse.message = "PESANAN BELUM TERKONFIRMASI";
-//               res.status(200).json(templateResponse);
-//             }
-//           } catch (error) {
-//             const templateResponse = responseTemplate.error;
-//             templateResponse.message = `${error}`;
-//             res.status(200).json(templateResponse);
-//           }
-//         } else {
-//           const templateResponse = responseTemplate.error;
-//           templateResponse.message = "PRODUCT NOT FOUND";
-//           res.status(200).json(templateResponse);
-//         }
-//       } catch (error) {
-//         const templateResponse = responseTemplate.error;
-//         templateResponse.message = `${error}`;
-//         res.status(200).json(templateResponse);
-//       }
-//     } else {
-//       const templateResponse = responseTemplate.error;
-//       templateResponse.message = "TRANSACTION NOT FOUND";
-//       res.status(200).json(templateResponse);
-//     }
-//   } catch (error) {
-//     const templateResponse = responseTemplate.error;
-//     templateResponse.message = `${error}`;
-//     res.status(200).json(templateResponse);
-//   }
-// });
+router.put("/completion/:id", async (req, res) => {
+  try {
+    const FindTransactionById = await Transaction.findById(req.params.id);
+    if (!FindTransactionById) {
+      const templateResponse = responseTemplate.error;
+      templateResponse.message = "TRANSACTION NOT FOUND";
+      res.status(200).json(templateResponse);
+      return;
+    }
+    if (FindTransactionById.status !== "2") {
+      const templateResponse = responseTemplate.error;
+      templateResponse.message = "TRANSACTION NOT CONFIRMED";
+      res.status(200).json(templateResponse);
+      return;
+    }
+    try {
+      const FindProductById = await Product.findById(
+        FindTransactionById.product
+      );
+      if (!FindProductById) {
+        const templateResponse = responseTemplate.error;
+        templateResponse.message = "PRODUCT NOT FOUND";
+        res.status(200).json(templateResponse);
+        return;
+      }
+      try {
+        const FindUserSellerById = await User.findById(FindProductById.seller);
+        if (!FindUserSellerById) {
+          const templateResponse = responseTemplate.error;
+          templateResponse.message = "SELLER NOT FOUND";
+          res.status(200).json(templateResponse);
+          return;
+        }
+        try {
+          const UpdateWalletSeller = await Wallet.findByIdAndUpdate(
+            FindUserSellerById.wallet,
+            { $inc: { balance: (+FindProductById.price * 98) / 100 } }
+          );
+          if (!UpdateWalletSeller) {
+            const templateResponse = responseTemplate.error;
+            templateResponse.message = "FAILED TO UPDATE SELLER WALLET";
+            res.status(200).json(templateResponse);
+            return;
+          }
+          try {
+            const FindResiById = await Resi.findById(FindTransactionById.resi);
+            if (!FindResiById) {
+              const templateResponse = responseTemplate.error;
+              templateResponse.message = `RESI NOT FOUND ${FindTransactionById.resi}`;
+              res.status(200).json(templateResponse);
+              return;
+            }
+            try {
+              const FindUserDriverById = await User.findById(
+                FindResiById.driver
+              );
+              if (!FindUserDriverById) {
+                const templateResponse = responseTemplate.error;
+                templateResponse.message = "DRIVER NOT FOUND";
+                res.status(200).json(templateResponse);
+                return;
+              }
+              try {
+                const UpdateWalletDriver = await Wallet.findByIdAndUpdate(
+                  FindUserDriverById.wallet,
+                  {
+                    $inc: {
+                      balance: (+FindTransactionById.deliveryCost * 95) / 100,
+                    },
+                  }
+                );
+                if (!UpdateWalletDriver) {
+                  const templateResponse = responseTemplate.error;
+                  templateResponse.message = "FAILED TO UPDATE WALLET DRIVER";
+                  res.status(200).json(templateResponse);
+                  return;
+                }
+                try {
+                  const FindAdminByUserType = await User.findOne({
+                    userType: 1,
+                  });
+                  if (!FindAdminByUserType) {
+                    const templateResponse = responseTemplate.error;
+                    templateResponse.message = "ADMIN NOT FOUND";
+                    res.status(200).json(templateResponse);
+                    return;
+                  }
+                  try {
+                    const UpdateWalletAdmin = await Wallet.findByIdAndUpdate(
+                      FindAdminByUserType.wallet,
+                      {
+                        $inc: {
+                          balance:
+                            (+FindTransactionById.deliveryCost * 5) / 100 +
+                            FindTransactionById.adminFee +
+                            (FindProductById.price * 2) / 100,
+                        },
+                      }
+                    );
+                    if (!UpdateWalletAdmin) {
+                      const templateResponse = responseTemplate.error;
+                      templateResponse.message =
+                        "FAILED TO UPDATE WALLET DRIVER";
+                      res.status(200).json(templateResponse);
+                      return;
+                    }
+                    try {
+                      const SetOrderStatus = await Transaction.findByIdAndUpdate(
+                        req.params.id,
+                        {
+                          status: "3",
+                        }
+                      );
+                      if (!SetOrderStatus) {
+                        const templateResponse = responseTemplate.error;
+                        templateResponse.message = "FAILED UPDATE ORDER STATUS";
+                        res.status(200).json(templateResponse);
+                        return;
+                      }
+                      const templateResponse = responseTemplate.success;
+                      templateResponse.data = SetOrderStatus;
+                      res.status(200).json(templateResponse);
+                    } catch (error) {
+                      const templateResponse = responseTemplate.error;
+                      templateResponse.message = `${error}`;
+                      res.status(200).json(templateResponse);
+                    }
+                  } catch (error) {
+                    const templateResponse = responseTemplate.error;
+                    templateResponse.message = `${error}`;
+                    res.status(200).json(templateResponse);
+                  }
+                } catch (error) {
+                  const templateResponse = responseTemplate.error;
+                  templateResponse.message = `${error}`;
+                  res.status(200).json(templateResponse);
+                }
+              } catch (error) {
+                const templateResponse = responseTemplate.error;
+                templateResponse.message = `${error}`;
+                res.status(200).json(templateResponse);
+              }
+            } catch (error) {
+              const templateResponse = responseTemplate.error;
+              templateResponse.message = `${error}`;
+              res.status(200).json(templateResponse);
+            }
+          } catch (error) {
+            const templateResponse = responseTemplate.error;
+            templateResponse.message = `${error}`;
+            res.status(200).json(templateResponse);
+          }
+        } catch (error) {
+          const templateResponse = responseTemplate.error;
+          templateResponse.message = `${error}`;
+          res.status(200).json(templateResponse);
+        }
+      } catch {
+        const templateResponse = responseTemplate.error;
+        templateResponse.message = `${error}`;
+        res.status(200).json(templateResponse);
+      }
+    } catch (error) {
+      const templateResponse = responseTemplate.error;
+      templateResponse.message = `${error}`;
+      res.status(200).json(templateResponse);
+    }
+  } catch (error) {
+    const templateResponse = responseTemplate.error;
+    templateResponse.message = `${error}`;
+    res.status(200).json(templateResponse);
+  }
+});
 
 router.put("/procces/:id", async (req, res) => {
   try {
@@ -166,7 +343,7 @@ router.put("/procces/:id", async (req, res) => {
                       req.params.id,
                       {
                         status: "2",
-                        resi: DBUserFindDriver._id,
+                        resi: DBResiInteraction._id,
                       }
                     );
                     if (DBUpdateTransactionInteraction) {
